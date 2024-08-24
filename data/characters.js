766662ce2adcf7222f360b3d11958ee317cce03a
Fisnik <71096106+Doommafia@users.noreply.github.com>
2024-08-24 15:52:24 +0200

    Add files via upload

---
+++ data/characters.js
@@ -0,0 +1,117 @@
+const charData = {
+    "characters": [
+        {
+            "Element": "Anemo",
+            "Characters": [
+                {"Name": "Jean", "Weapon": "Sword"},
+                {"Name": "Kaedehara Kazuha", "Weapon": "Sword"},
+                {"Name": "Lynette", "Weapon": "Sword"},
+                {"Name": "Sayu", "Weapon": "Claymore"},
+                {"Name": "Traveler (Anemo)", "Weapon": "Sword"},
+                {"Name": "Venti", "Weapon": "Bow"},
+                {"Name": "Wanderer", "Weapon": "Catalyst"},
+                {"Name": "Xianyun", "Weapon": "Catalyst"}
+            ]
+        },
+        {
+            "Element": "Cryo",
+            "Characters": [
+                {"Name": "Amber", "Weapon": "Bow"},
+                {"Name": "Ayaka", "Weapon": "Sword"},
+                {"Name": "Barbara", "Weapon": "Catalyst"},
+                {"Name": "Chongyun", "Weapon": "Claymore"},
+                {"Name": "Diona", "Weapon": "Bow"},
+                {"Name": "Eula", "Weapon": "Claymore"},
+                {"Name": "Freminet", "Weapon": "Claymore"},
+                {"Name": "Hu Tao", "Weapon": "Polearm"},
+                {"Name": "Klee", "Weapon": "Catalyst"},
+                {"Name": "Mika", "Weapon": "Polearm"},
+                {"Name": "Qiqi", "Weapon": "Sword"},
+                {"Name": "Rosaria", "Weapon": "Polearm"},
+                {"Name": "Shenhe", "Weapon": "Polearm"},
+                {"Name": "Wriothesley", "Weapon": "Catalyst"},
+                {"Name": "Yanfei", "Weapon": "Catalyst"},
+                {"Name": "Yun Jin", "Weapon": "Polearm"}
+            ]
+        },
+        {
+            "Element": "Dendro",
+            "Characters": [
+                {"Name": "Alhaitham", "Weapon": "Sword"},
+                {"Name": "Baizhu", "Weapon": "Catalyst"},
+                {"Name": "Collei", "Weapon": "Bow"},
+                {"Name": "Emilie", "Weapon": "Polearm"},
+                {"Name": "Kaveh", "Weapon": "Claymore"},
+                {"Name": "Kirara", "Weapon": "Sword"},
+                {"Name": "Nahida", "Weapon": "Catalyst"},
+                {"Name": "Yaoyao", "Weapon": "Polearm"}
+            ]
+        },
+        {
+            "Element": "Electro",
+            "Characters": [
+                {"Name": "Beidou", "Weapon": "Claymore"},
+                {"Name": "Bennett", "Weapon": "Sword"},
+                {"Name": "Dori", "Weapon": "Claymore"},
+                {"Name": "Keqing", "Weapon": "Sword"},
+                {"Name": "Lisa", "Weapon": "Catalyst"},
+                {"Name": "Raiden", "Weapon": "Polearm"},
+                {"Name": "Sara", "Weapon": "Bow"},
+                {"Name": "Shinobu", "Weapon": "Sword"},
+                {"Name": "Sethos", "Weapon": "Bow"},
+                {"Name": "Traveler (Electro)", "Weapon": "Sword"},
+                {"Name": "Yae Miko", "Weapon": "Catalyst"}
+            ]
+        },
+        {
+            "Element": "Geo",
+            "Characters": [
+                {"Name": "Albedo", "Weapon": "Sword"},
+                {"Name": "Chiori", "Weapon": "Sword"},
+                {"Name": "Gorou", "Weapon": "Bow"},
+                {"Name": "Itto", "Weapon": "Claymore"},
+                {"Name": "Navia", "Weapon": "Claymore"},
+                {"Name": "Ningguang", "Weapon": "Catalyst"},
+                {"Name": "Noelle", "Weapon": "Claymore"},
+                {"Name": "Traveler (Geo)", "Weapon": "Sword"},
+                {"Name": "Yun Jin", "Weapon": "Polearm"},
+                {"Name": "Zhongli", "Weapon": "Polearm"}
+            ]
+        },
+        {
+            "Element": "Hydro",
+            "Characters": [
+                {"Name": "Ayato", "Weapon": "Sword"},
+                {"Name": "Barbara", "Weapon": "Catalyst"},
+                {"Name": "Furina", "Weapon": "Sword"},
+                {"Name": "Kokomi", "Weapon": "Catalyst"},
+                {"Name": "Mona", "Weapon": "Catalyst"},
+                {"Name": "Neuvillette", "Weapon": "Catalyst"},
+                {"Name": "Nilou", "Weapon": "Sword"},
+                {"Name": "Sigewinne", "Weapon": "Bow"},
+                {"Name": "Tartaglia", "Weapon": "Bow"},
+                {"Name": "Traveler (Hydro)", "Weapon": "Sword"},
+                {"Name": "Yelan", "Weapon": "Bow"},
+                {"Name": "Yoimiya", "Weapon": "Bow"}
+            ]
+        },
+        {
+            "Element": "Pyro",
+            "Characters": [
+                {"Name": "Aloy", "Weapon": "Bow"},
+                {"Name": "Amber", "Weapon": "Bow"},
+                {"Name": "Arlecchino", "Weapon": "Polearm"},
+                {"Name": "Candace", "Weapon": "Polearm"},
+                {"Name": "Charlotte", "Weapon": "Catalyst"},
+                {"Name": "Chevreuse", "Weapon": "Polearm"},
+                {"Name": "Diluc", "Weapon": "Claymore"},
+                {"Name": "Dori", "Weapon": "Claymore"},
+                {"Name": "Gaming", "Weapon": "Claymore"},
+                {"Name": "Ganyu", "Weapon": "Bow"},
+                {"Name": "Xiangling", "Weapon": "Polearm"},
+                {"Name": "Xinyan", "Weapon": "Claymore"},
+                {"Name": "Yoimiya", "Weapon": "Bow"}
+            ]
+        }
+    ]
+};
