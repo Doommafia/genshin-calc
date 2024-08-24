d684f0a39f553e69ad0a42d8912319f623ecc242
Fisnik <71096106+Doommafia@users.noreply.github.com>
2024-08-24 15:51:11 +0200

    Update characters.js

--- data/characters.js
+++ data/characters.js
@@ -1,118 +1,121 @@
-[
-    {
-        "Element": "Anemo",
-        "Characters": [
-            "Jean",
-            "Kaedehara Kazuha",
-            "Lynette",
-            "Sayu",
-            "Traveler (Anemo)",
-            "Venti",
-            "Wanderer",
-            "Xianyun"
-        ]
-    },
-    {
-        "Element": "Cryo",
-        "Characters": [
-            "Amber",
-            "Ayaka",
-            "Barbara",
-            "Chongyun",
-            "Diona",
-            "Eula",
-            "Faruzan",
-            "Fischl",
-            "Freminet",
-            "Hu Tao",
-            "Klee",
-            "Mika",
-            "Qiqi",
-            "Rosaria",
-            "Shenhe",
-            "Wriothesley",
-            "Yanfei",
-            "Yun Jin",
-            "Zhongli"
-        ]
-    },
-    {
-        "Element": "Dendro",
-        "Characters": [
-            "Alhaitham",
-            "Baizhu",
-            "Collei",
-            "Emilie",
-            "Kaveh",
-            "Kirara",
-            "Nahida",
-            "Yaoyao"
-        ]
-    },
-    {
-        "Element": "Electro",
-        "Characters": [
-            "Beidou",
-            "Bennett",
-            "Dori",
-            "Lisa",
-            "Raiden",
-            "Sara",
-            "Shinobu",
-            "Sethos",
-            "Traveler (Electro)",
-            "Yae Miko"
-        ]
-    },
-    {
-        "Element": "Geo",
-        "Characters": [
-            "Albedo",
-            "Chiori",
-            "Gorou",
-            "Itto",
-            "Navia",
-            "Ningguang",
-            "Noelle",
-            "Traveler (Geo)",
-            "Yun Jin",
-            "Zhongli"
-        ]
-    },
-    {
-        "Element": "Hydro",
-        "Characters": [
-            "Ayato",
-            "Barbara",
-            "Furina",
-            "Kokomi",
-            "Mona",
-            "Neuvillette",
-            "Nilou",
-            "Sigewinne",
-            "Traveler (Hydro)",
-            "Yelan",
-            "Yoimiya"
-        ]
-    },
-    {
-        "Element": "Pyro",
-        "Characters": [
-            "Aloy",
-            "Amber",
-            "Arlecchino",
-            "Bennett",
-            "Candace",
-            "Charlotte",
-            "Chevreuse",
-            "Diluc",
-            "Dori",
-            "Gaming",
-            "Ganyu",
-            "Xiangling",
-            "Xinyan",
-            "Yanfei",
-            "Yoimiya"
-        ]
-    }
-]
+const charData = {
+    "characters": [
+        {
+            "Element": "Anemo",
+            "Characters": [
+                "Jean",
+                "Kaedehara Kazuha",
+                "Lynette",
+                "Sayu",
+                "Traveler (Anemo)",
+                "Venti",
+                "Wanderer",
+                "Xianyun"
+            ]
+        },
+        {
+            "Element": "Cryo",
+            "Characters": [
+                "Amber",
+                "Ayaka",
+                "Barbara",
+                "Chongyun",
+                "Diona",
+                "Eula",
+                "Faruzan",
+                "Fischl",
+                "Freminet",
+                "Hu Tao",
+                "Klee",
+                "Mika",
+                "Qiqi",
+                "Rosaria",
+                "Shenhe",
+                "Wriothesley",
+                "Yanfei",
+                "Yun Jin",
+                "Zhongli"
+            ]
+        },
+        {
+            "Element": "Dendro",
+            "Characters": [
+                "Alhaitham",
+                "Baizhu",
+                "Collei",
+                "Emilie",
+                "Kaveh",
+                "Kirara",
+                "Nahida",
+                "Yaoyao"
+            ]
+        },
+        {
+            "Element": "Electro",
+            "Characters": [
+                "Beidou",
+                "Bennett",
+                "Dori",
+                "Lisa",
+                "Raiden",
+                "Sara",
+                "Shinobu",
+                "Sethos",
+                "Traveler (Electro)",
+                "Yae Miko"
+            ]
+        },
+        {
+            "Element": "Geo",
+            "Characters": [
+                "Albedo",
+                "Chiori",
+                "Gorou",
+                "Itto",
+                "Navia",
+                "Ningguang",
+                "Noelle",
+                "Traveler (Geo)",
+                "Yun Jin",
+                "Zhongli"
+            ]
+        },
+        {
+            "Element": "Hydro",
+            "Characters": [
+                "Ayato",
+                "Barbara",
+                "Furina",
+                "Kokomi",
+                "Mona",
+                "Neuvillette",
+                "Nilou",
+                "Sigewinne",
+                "Traveler (Hydro)",
+                "Yelan",
+                "Yoimiya"
+            ]
+        },
+        {
+            "Element": "Pyro",
+            "Characters": [
+                "Aloy",
+                "Amber",
+                "Arlecchino",
+                "Bennett",
+                "Candace",
+                "Charlotte",
+                "Chevreuse",
+                "Diluc",
+                "Dori",
+                "Gaming",
+                "Ganyu",
+                "Xiangling",
+                "Xinyan",
+                "Yanfei",
+                "Yoimiya"
+            ]
+        }
+    ]
+    
+}
